48b59ec6f8b573516720ad5c984435eb8b9e804a
Jason Cobb <jason.e.cobb@gmail.com>
2018-10-25 17:28:48 -0400

    Changes required to work in robotics repo

--- openCVLibrary3/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary3/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: E:\\Programs\\FTC\\Projects\\uWu-Bot\\openCVLibrary3\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: /home/meowingtwurtle/AndroidStudioProjects/robotics/DogeCV/openCVLibrary3/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
